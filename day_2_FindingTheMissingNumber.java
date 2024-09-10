1d902824c75e3cd688e619379e7c9a50cda54142
Deep Dave <97455623+deepdave22@users.noreply.github.com>
2024-09-10 23:40:34 +0530

    Update day_2_FindingTheMissingNumber.java

--- day_2_FindingTheMissingNumber.java
+++ day_2_FindingTheMissingNumber.java
@@ -1,1 +1,11 @@
+public class day_2_FindingTheMissingNumber{
+  public static int findNumber(int[] arr){
+    int n = arr.length + 1;
+    long Sum_of_all_numbers = (long)n*(n+1)/2;
+    long sum_we_got=0;
 
+    for(int num : arr){
+        sum_we_got +=num;
+     }
+    return (int) (Sum_of_all_numbers - sum_we_got);
+}
