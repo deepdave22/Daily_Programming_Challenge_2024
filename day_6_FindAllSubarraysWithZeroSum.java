e80bbbfbf16252e584de0a5ae1cefa0661228266
Deep Dave <97455623+deepdave22@users.noreply.github.com>
2024-09-14 20:32:14 +0530

    Update day_6_FindAllSubarraysWithZeroSum.java

--- day_6_FindAllSubarraysWithZeroSum.java
+++ day_6_FindAllSubarraysWithZeroSum.java
@@ -1,1 +1,29 @@
+//we are using hashmap and the concept of prefixSum
 
+import java.util.*;
+
+public class ZeroSumSubarrays {
+    public static List<int[]> findZeroSumSubarrays(int[] nums) {
+        List<int[]> results = new ArrayList<>();
+        Map<Long, List<Integer>> sumToIndices = new HashMap<>();
+        
+        sumToIndices.put(0L, new ArrayList<>(Arrays.asList(-1)));
+        
+        long sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            
+            if (sumToIndices.containsKey(sum)) {
+                List<Integer> starts = sumToIndices.get(sum);
+                for (int start : starts) {
+                    results.add(new int[]{start + 1, i});
+                }
+            }
+            
+            sumToIndices.computeIfAbsent(sum, k -> new ArrayList<>()).add(i);
+        }
+        
+        return results;
+    }
+}
